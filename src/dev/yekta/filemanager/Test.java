5b7bfc41d2dd8aee139d94ed67e97ccc5a2313de
Ali Khaleqi Yekta <Me@Yekta.Dev>
2021-06-07 14:56:21 +0430

    Add tests and configs

---
+++ src/dev/yekta/filemanager/Test.java
@@ -0,0 +1,29 @@
+package dev.yekta.filemanager;
+
+public class Test {
+    public static void main(String[] args) {
+        FileManager<TestObject> file = new FileManager<>(TestObject::new, "data.dat");
+        file.write(new TestObject("A", 18, 20f, 20.00d, true));
+        file.write(new TestObject("B", 18, 20f, 20.00d, true));
+        file.write(new TestObject("C", 18, 20f, 20.00d, true));
+        file.write(new TestObject("D", 18, 20f, 20.00d, true));
+        file.write(new TestObject("E", 18, 20f, 20.00d, true));
+        file.write(new TestObject("F", 18, 20f, 20.00d, true));
+        file.seekRecord(2);
+        file.write(new TestObject("Supposed to be C", 18, 20f, 20.00d, true));
+        file.close();
+
+        file = new FileManager<>(TestObject::new, "data.dat");
+
+        //file.seekRecord(3);
+        //file.delete();
+        //file.append(new Obj("YYYY",88,20.8f,288.00d,false));
+
+        file.swap(0, 5); // A <-> F
+
+        for (TestObject o : file.readStartToEnd())
+            System.out.println(o);
+
+        file.close();
+    }
+}
---
+++ src/dev/yekta/filemanager/TestObject.java
@@ -0,0 +1,56 @@
+package dev.yekta.filemanager;
+
+import java.util.List;
+
+public class TestObject implements FileSerializable<TestObject> {
+    private String name;
+    private int age;
+    private float grade;
+    private double bigGrade;
+    private boolean isHappy;
+
+    public TestObject(String name, int age, float grade, double bigGrade, boolean isHappy) {
+        this.name = name;
+        this.age = age;
+        this.grade = grade;
+        this.bigGrade = bigGrade;
+        this.isHappy = isHappy;
+    }
+
+    public TestObject() {
+
+    }
+
+    @Override
+    public SerializableField[] serialize() {
+        return new SerializableField[]{
+                new SerializableField(name),
+                new SerializableField(age),
+                new SerializableField(grade),
+                new SerializableField(bigGrade),
+                new SerializableField(isHappy),
+        };
+    }
+
+    @Override
+    public TestObject deserialize(List<SerializableField> data) {
+        return new TestObject(
+                (String) data.get(0).getField(),
+                (int) data.get(1).getField(),
+                (float) data.get(2).getField(),
+                (double) data.get(3).getField(),
+                (boolean) data.get(4).getField()
+        );
+    }
+
+    @Override
+    public String toString() {
+        return "Obj{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", grade=" + grade +
+                ", bigGrade=" + bigGrade +
+                ", isHappy=" + isHappy +
+                '}';
+    }
+}
